63bff023d84d1374389658ee171a816ce164317d
AdrianCaMu <a_adrian.camara.munoz@iespablopicasso.es>
2022-04-01 11:38:38 +0200

    Version 1

---
+++ src/main/MainApp.java
@@ -0,0 +1,12 @@
+package main;
+
+/**
+ * Hola mundo
+ * @author Adrian Camara Muñoz
+ *
+ */
+public class MainApp {
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+	}
+}
